613cfeec9407212bef1155d93a82955bfd994ebb
Raleigh D <RDFroeber@gmail.com>
2013-11-26 13:20:43 -0500

    Delete RoundTribute Join Table
    Tributes are now types of Citizen

--- db/migrate/20131125214148_create_join_table_round_tribute.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableRoundTribute < ActiveRecord::Migration
-  def change
-    create_join_table :rounds, :tributes do |t|
-      t.index [:round_id, :tribute_id]
-      t.index [:tribute_id, :round_id]
-    end
-  end
-end
