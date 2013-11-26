ddd54fe52bbd732b1dd768b06c913a6a9c97b493
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-26 18:09:25 -0500

    Reapee selection updated

--- app/models/gamemaker.rb
+++ app/models/gamemaker.rb
@@ -6,7 +6,7 @@ class Gamemaker
   attr_reader :game
 
   def reap_citizens
-    reapees = Citizen.all.where(age: 12..18)
-    
+    reapees = Citizen.all.where(age: 12..18, tesserae: 1..30)
+    # 1 F and 1 M from each district
   end
 end
