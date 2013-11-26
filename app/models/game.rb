ff1e162a7caed17ee6dc8cff5463d7f5ee2a5c9b
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-25 23:41:53 -0500

    Gamemaker model and rspec added

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,15 @@
 class Game < ActiveRecord::Base
   has_many :rounds
-  
+
   validates :president, presence: true
   validates :name, presence: true
 
+  after_save :prepare_for_game
+
+  def prepare_for_game 
+    gamemaker = Gamemaker.new(self)
+    # gamemaker.some_method
+
+    gamemaker
+  end
 end
---
+++ app/models/gamemaker.rb
@@ -0,0 +1,11 @@
+class Gamemaker
+  def initialize(game)
+    @game = game
+  end
+
+  attr_reader :game
+
+  # def some_method
+
+  # end
+end
