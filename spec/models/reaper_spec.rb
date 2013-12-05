8c0f29ca2db6d00465e9d1caeda7794650778f54
Raleigh D Froeber <RDFroeber@gmail.com>
2013-12-04 23:10:42 -0500

    testing #select_tributes added for number and gender

--- spec/models/reaper_spec.rb
+++ spec/models/reaper_spec.rb
@@ -19,5 +19,17 @@ describe Reaper do
     it "chooses 24 tributes" do
       expect(reap.select_tributes.count).to eq(24)
     end
+
+    it "selects 12 of each gender" do
+      expect(reap.select_tributes.select{|trib| trib.gender == "F"}.count).to eq(12)
+      expect(reap.select_tributes.select{|trib| trib.gender == "M"}.count).to eq(12)
+    end
+
+    it "selects 2 from each district" do
+      grouped_tribs = reap.select_tributes.group_by {|trib| trib.district_id}
+      grouped_tribs.each do |district,arr| 
+        expect(arr.count).to eq(2)
+      end
+    end
   end
 end
